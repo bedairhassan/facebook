b02a253906e1405a43957d9e7f7e539462791f25
bedairhassan <hassan.hossam.bedair@gmail.com>
2021-03-03 23:30:15 +0200

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Route,  BrowserRouter as Router,Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+        <Route exact path="/person">
+          <Person/>
+          </Route>
+
+          <Route exact path="/people">
+          <People/>
+          </Route>
+      </Switch>
+      </Router>
     </div>
   );
+}
+
+function People() {
+  return (
+    <h1>People</h1>
+  )
+}
+
+function Person() {
+  return (
+    <h1>Person</h1>
+  )
 }
 
 export default App;
