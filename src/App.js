4bab9d5ba8e11dd13a18eeb4721c7504efdfb7c2
bedairhassan <hassan.hossam.bedair@gmail.com>
2021-03-03 23:35:09 +0200

    added Login page with checkbox as demonstration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import { Route,  BrowserRouter as Router,Switch, Redirect } from 'react-router-dom';
+import React, { useState } from "react"
+import { Route, BrowserRouter as Router, Switch, Redirect, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Switch>
-        <Route exact path="/person">
-          <Person/>
+      <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/person">Person</Link>
+        </li>
+        <li>
+          <Link to="/people">People</Link>
+        </li>
+        <Switch>
+        <Route exact path="/login">
+            <Login />
           </Route>
-
+          <Route exact path="/person">
+            <Person />
+          </Route>
           <Route exact path="/people">
-          <People/>
+            <People />
           </Route>
-      </Switch>
+        </Switch>
       </Router>
     </div>
   );
+}
+
+function Login() {
+
+  const [isLogin,isLoginset]=useState(false)
+
+  function loginAction(){
+    isLoginset(!isLogin)
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Login Page</h1>
+      <input type="checkbox" onChange={()=>loginAction()}/>
+      {isLogin===true?`logged in `:`NOT`}
+    </React.Fragment>
+
+  )
 }
 
 function People() {
