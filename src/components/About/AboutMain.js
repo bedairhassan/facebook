1e927da21d740a8b37b08138c4ad45ac971ffbd3
bedairhassan <hassan.hossam.bedair@gmail.com>
2021-03-04 12:30:55 +0200

    DisplayIf

--- src/components/About/AboutMain.js
+++ src/components/About/AboutMain.js
@@ -9,20 +9,30 @@ import Plain from './Plain'
 import editIconYES from '../../images/edit_32px_yes.png'
 import editIconNO from '../../images/edit_32px_no.png'
 
+import DisplayIf from '../../reusable-components/DisplayIf'
 
 export default function About() {
 
-    const [isEditing,isEditingset]=useState(false)
+  const [isEditing, isEditingset] = useState(false)
 
-    const [user,userSet]=useState({}) // name, phone_adress, email_address
+  const [user, userSet] = useState({}) // name, phone_adress, email_address
 
-    return (
-      <React.Fragment>
-        <h1>About Page</h1>
+  return (
+    <React.Fragment>
+      <h1>About Page</h1>
 
-        {isEditing?<img src={editIconYES} onClick={()=>isEditingset(!isEditing)}/>:<img src={editIconNO} onClick={()=>isEditingset(!isEditing)}/>}
-        {isEditing?<Editing/>:<Plain user={user}/>}
-      </React.Fragment>
-    )
-  }
-  
+
+
+      <DisplayIf
+        yes={editIconYES}
+        no={editIconNO}
+        Trigger={() => isEditingset(!isEditing)}
+        condition={isEditing} />
+
+
+
+
+      {isEditing ? <Editing /> : <Plain user={user} />}
+    </React.Fragment>
+  )
+}
---
+++ src/reusable-components/DisplayIf.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DisplayIf = ({ yes, no, Trigger, condition }) => {
+    return (
+        <div>
+
+            {condition ?
+                <img src={yes} onClick={Trigger} />
+                : <img src={no} onClick={Trigger} />}
+
+        </div>
+    );
+};
+
+export default DisplayIf;
