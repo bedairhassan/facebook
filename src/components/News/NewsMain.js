e930acc72b7cb27f02df51bc2c8d2d669556de66
bedairhassan <hassan.hossam.bedair@gmail.com>
2021-03-06 13:18:41 +0200

    NewsMain.js

--- src/components/News/NewsMain.js
+++ src/components/News/NewsMain.js
@@ -7,19 +7,9 @@ const NewsMain = () => {
 
     const [data, dataSet] = useState([])
 
-    // load from firebase!!!!
+    // load from firebase then sort. 
     useEffect(() => {
-        // dataSet([
-        //     { type: `post`, text: `post4`, user: `user919`, date: `9-1-2019` },
-        //     { type: `news`, text: `news1`, user: `user919`, date: `9-1-2020` },
-        //     { type: `news`, text: `news2`, user: `user919`, date: `9-1-2029` },
-        //     { type: `news`, text: `news3`, user: `user919`, date: `9-1-2029` },
-        //     { type: `post`, text: `post1`, user: `user919`, date: `9-1-2029` },
-        //     { type: `post`, text: `post2`, user: `user919`, date: `9-1-2029` },
-        //     { type: `post`, text: `post3`, user: `user919`, date: `9-1-2029` }
-        // ])
-
-        // sort by NEWS FIRST 
+        
         let data = sortByNewsFirst([
             { type: `post`, text: `post4`, user: `user919`, date: `9-1-2019` },
             { type: `news`, text: `news1`, user: `user919`, date: `9-1-2020` },
@@ -52,8 +42,8 @@ function Card({ card }) {
         <React.Fragment>
             <div className="card">
                 <h2>{card[`text`]}</h2>
-                <h5>{card[`type`]} posted at {card[`date`]}</h5>
-                <p>{card[`user`]}</p>
+                <h5 align="right">{card[`type`]} posted at {card[`date`]}</h5>
+                <p align="left">{card[`user`]}</p>
             </div>
         </React.Fragment>
     )
